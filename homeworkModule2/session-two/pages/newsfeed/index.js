dafa0258fbc7c6cda959ae6bac7b03a8dd333d6a
wyntherpanther <wynniew60@gmail.com>
2022-03-23 18:06:52 +0700

    cleaning up folders

---
+++ homeworkModule2/session-two/pages/newsfeed/index.js
@@ -0,0 +1,19 @@
+const NewsFeedPage =()=>
+return(
+    <div className ="container">
+        <div className="feeds">
+        <div className="feeds_group">
+        <div className="feeds_categories">
+        <div className="feeds_wrapper">
+        <div className="feeds_content">
+
+        </div>
+        <div className="feeds_image">
+            image
+        </div>
+        </div>
+        </div>
+        </div>
+        
+    </div>
+)
